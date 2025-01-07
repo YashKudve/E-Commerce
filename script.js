2c8559155b3db27fbd3f59a7225f732331617455
Yash <kudveyash@gmail.com>
2025-01-07 17:17:02 +0530

    new arrival products section and modifications

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-async function loadFeaturedProducts() {
+async function loadProducts() {
     try {
+        
         const response = await fetch('featured-product.json');
-        const products = await response.json();
+        const data = await response.json();
 
-        const container = document.getElementById('pro-container')
+        populateProducts('featured-container', data.featuredProducts);
 
-        products.forEach(product => {
-            const productHTML = `
-                <div class="pro">
-                    <img src="${product.image}" alt="${product.name}">
-                    <div class="desc">
-                        <span>${product.brand}</span>
-                        <h5>${product.name}</h5>
-                        <div class="star">
-                            ${'<i class="fas fa-star"></i>'.repeat(product.rating)}
-                            ${'<i class="far fa-star"></i>'.repeat(5 - product.rating)}
-                        </div>
-                        <h4>$${product.price}</h4>
-                    </div>
-                    <a href="#" class="fal fa-shopping-cart cart"></a>
-                </div>
-            `;
-            container.innerHTML += productHTML;
-        })
+        populateProducts('new-arrivals-container', data.newArrivals);
     } catch (error) {
         console.error('Error loading products:', error);
     }
 }
 
-loadFeaturedProducts();
+function populateProducts(containerId, products) {
+    const container = document.getElementById(containerId);
+
+    products.forEach(product => {
+        const productHTML = `
+            <div class="pro">
+                <img src="${product.image}" alt="${product.name}">
+                <div class="desc">
+                    <span>${product.brand}</span>
+                    <h5>${product.name}</h5>
+                    <div class="star">
+                        ${'<i class="fas fa-star"></i>'.repeat(product.rating)}
+                        ${'<i class="far fa-star"></i>'.repeat(5 - product.rating)}
+                    </div>
+                    <h4>$${product.price}</h4>
+                </div>
+                <a href="#" class="fal fa-shopping-cart cart"></a>
+            </div>
+        `;
+        container.innerHTML += productHTML;
+    });
+}
+
+loadProducts();
